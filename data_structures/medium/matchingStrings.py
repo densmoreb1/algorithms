6410fed6f68fe0275a8247b1eed510bc2e38bed7
Brandon Densmore <78512643+densmoreb1@users.noreply.github.com>
2022-03-19 15:03:26 -0600

    finshed solution

---
+++ data_structures/medium/matchingStrings.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=false
+# given an array and a list of queries
+# return a count list of how many are found
+
+def matchingStrings(strings, queries):
+    count = []
+    
+    for i in queries:
+        count.append(strings.count(i))
+    return count
+
+
+strings = ['aba', 'baba', 'aba', 'xzxb']
+queries = ['aba', 'xzxb','ab']
+
+print(matchingStrings(strings, queries))
