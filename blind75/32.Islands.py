27d571017762057e4512603324658dfe393f07e5
Brandon Densmore <78512643+densmoreb1@users.noreply.github.com>
2022-04-01 09:28:12 -0600

    islands

---
+++ blind75/32.Islands.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/number-of-islands/submissions/
+
+# use bfs to mark 1s as visted before increasing number of islands
+import collections
+def numIslands(grid) -> int:
+        
+    max_rows, max_cols = len(grid), len(grid[0])
+    visited = set()
+    islands = 0
+    
+    
+    def bfs(r, c):
+        
+        q = collections.deque()
+        visited.add((r,c))
+        q.append((r,c))
+        
+        while q:
+            row, col = q.popleft()
+            poss_dir = [[1,0], [-1,0], [0,1], [0,-1]]
+            
+            for dr, dc in poss_dir:
+                r, c = row + dr, col + dc
+                
+                if (r in range(max_rows) and c in range(max_cols) and grid[r][c] == '1' and (r,c) not in visited):
+                    visited.add((r,c))
+                    q.append((r,c))
+    
+
+    for row in range(max_rows):
+        for col in range(max_cols):
+            if grid[row][col] == "1" and (row,col) not in visited:
+                bfs(row, col)
+                islands += 1
+    
+    return islands
