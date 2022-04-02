c1345c08480d7f1cff961dc50c0b91d6f01b5ed4
Brandon Densmore <78512643+densmoreb1@users.noreply.github.com>
2022-04-02 15:02:47 -0600

    c

--- blind75/6.ValidParen.py
+++ blind75/6.ValidParen.py
@@ -8,12 +8,15 @@ def isValid(s: str) -> bool:
                     '}': '{'}
     
     for p in s:
+        # this only checks the second parenthese
         if p in paren_dict:
+            # if the last one matches what the key is then pop it
             if stack and stack[-1] == paren_dict[p]:
                 stack.pop()
             else:
                 return False
         else:
+            # appends the first one
             stack.append(p)
     
     return True if not stack else False
