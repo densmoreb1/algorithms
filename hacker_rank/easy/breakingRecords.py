5cb8cdb446fd9baa298663b32a61a63e87c27489
Brandon <densmoreb1@icloud.com>
2024-12-19 20:28:28 -0500

    format

--- hacker_rank/easy/breakingRecords.py
+++ hacker_rank/easy/breakingRecords.py
@@ -3,16 +3,16 @@
 
 def breakingRecords(scores):
     # probably keep track of a count that raises each time the max changes
-    
+
     min = max = scores[0]
     count_max = count_min = 0
-    
+
     for i in scores:
         if i > max:
             max = i
-            count_max += 1        
+            count_max += 1
         if i < min:
             min = i
             count_min += 1
-    
+
     return [count_max, count_min]
