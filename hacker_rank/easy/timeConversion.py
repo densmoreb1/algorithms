4afd5113d29fbc403abc0a5157b71db04fe191f4
Brandon <densmoreb1@icloud.com>
2024-12-19 20:31:34 -0500

    format

--- hacker_rank/easy/timeConversion.py
+++ hacker_rank/easy/timeConversion.py
@@ -7,19 +7,21 @@ def timeConversion(s):
     hh, mm, ss = [int(x) for x in s.split(":")]
 
     if AM_PM == 'PM' and hh != 12:
-        return('{:02}:{:02}:{:02}'.format(hh+12, mm, ss))
+        return ('{:02}:{:02}:{:02}'.format(hh + 12, mm, ss))
     elif AM_PM == 'AM' and hh == 12:
-        return('{:02}:{:02}:{:02}'.format(0, mm, ss))
+        return ('{:02}:{:02}:{:02}'.format(0, mm, ss))
     else:
-        return('{:02}:{:02}:{:02}'.format(hh, mm, ss))
+        return ('{:02}:{:02}:{:02}'.format(hh, mm, ss))
+
 
 def time2(s):
     ap = s[-2:]
     h, m, s = [int(x) for x in s[:8].split(':')]
-    
+
     if ap == "PM":
-        return f"{h%12+12:02}:{m:02}:{s:02}"
+        return f"{h % 12 + 12:02}:{m:02}:{s:02}"
     else:
-        return f"{h%12:02}:{m:02}:{s:02}"
+        return f"{h % 12:02}:{m:02}:{s:02}"
+
 
 print(time2('07:05:45PM'))
