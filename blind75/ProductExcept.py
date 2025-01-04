41eea90bc6dd0907dba649d3194a0bc3c63d888e
Brandon <densmoreb1@icloud.com>
2025-01-04 12:00:00 -0500

    formatting

--- blind75/ProductExcept.py
+++ blind75/ProductExcept.py
@@ -1,19 +1,19 @@
 #  https://leetcode.com/problems/product-of-array-except-self/submissions/
 
 # go through the list forwards finding the number multiplied forwards
-# then go through backwards finding 
+# then go through backwards finding
 def productExceptSelf(nums):
-        
+
     result = [1] * len(nums)
-    
+
     prefix = 1
     for i in range(len(nums)):
         result[i] = prefix
         prefix *= nums[i]
-    
+
     post = 1
-    for i in range(len(nums)-1, -1, -1):
+    for i in range(len(nums) - 1, -1, -1):
         result[i] *= post
         post *= nums[i]
-    
+
     return result
