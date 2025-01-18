d0c0bf5627d8a80c20baaed96dfa65cefd7917c3
Brandon <densmoreb1@icloud.com>
2025-01-17 20:05:15 -0500

    dfs

---
+++ leetcode/easy/MaximumDepthBinaryTree.py
@@ -0,0 +1,31 @@
+class Node:
+    # Classic binary tree
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def build_tree(nodes, f):
+    """this function builds a tree from input; you don't have to modify it"""
+    val = next(nodes)
+    if val == "x":
+        return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+
+def max_depth(root):
+    if root is not None:
+        # print(root.val)
+        left = max_depth(root.left)
+        right = max_depth(root.right)
+        return max(left, right) + 1
+    else:
+        return 0
+
+
+root = '3 9 20 x x 15 7'
+root = build_tree(iter('5 4 3 x x 8 x x 6 x x'.split()), int)
+print(max_depth(root))
